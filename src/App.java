16832c10aee70dd73e9bf80f267bdbfaa88c625c
IGOR <igor2311@gmail.com>
2022-10-04 23:41:45 -0300

    Corrigido main

--- src/App.java
+++ src/App.java
@@ -3,7 +3,6 @@ import List.GraphList;
 public class App {
     public static void main(String[] args) throws Exception {
         GraphList graph = new GraphList("./src/g1.txt");
-        graph.addEdge(0, 1, 2);
         System.out.println(graph);
 
     }
