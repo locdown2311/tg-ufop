f38d527211209eaf335c75e053e1d9c104472869
IGOR <igor2311@gmail.com>
2022-10-28 16:48:57 -0300

    Removido lixo

--- src/App.java
+++ src/App.java
@@ -9,16 +9,23 @@ public class App {
         String filename = args[0];
         String metodo = args[1];
         long[] duration = new long[1];
+        long startTime;
+        long endTime;
         GraphList graph = new GraphList(filename);
         switch (metodo) {
             case "dij":
-                long startTime = System.nanoTime();
+                startTime = System.nanoTime();
                 graph.djikstra(0,100);
-                long endTime = System.nanoTime();
+                endTime = System.nanoTime();
                 duration[0] = (endTime - startTime);
                 System.out.println("Tempo de execução Djikstra: " + duration[0] + "ns");
                 break;
             case "bf":
+                startTime = System.nanoTime();
+                graph.bellmanFord(0,100);
+                endTime = System.nanoTime();
+                duration[0] = (endTime - startTime);
+                System.out.println("Tempo de execução Bellman-Ford: " + duration[0] + "ns");
                 break;
             case "bfm":
                 break;
