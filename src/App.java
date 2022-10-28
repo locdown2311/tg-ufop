9b8bcb673f3b2984c1399ca2b799a14ec0494f19
IGOR <igor2311@gmail.com>
2022-10-28 19:56:45 -0300

    Caminho corrigido

--- src/App.java
+++ src/App.java
@@ -55,7 +55,7 @@ public class App {
             case "maze":
                 startTime = System.nanoTime();
                 GraphList maze = GraphList.asciiMazeToGraphList(filename);
-                maze.dfsRec(0);
+                caminho = maze.dfsRec(0);
                 endTime = System.nanoTime();
                 duration[0] = (endTime - startTime);
                 System.out.println("Tempo de execução Maze: " + duration[0] + "ns");
